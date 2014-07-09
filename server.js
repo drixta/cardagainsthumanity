9999c9eedf594d86b090c8cabc12e509a77e0866
Pham <quapham@uss11quphammb.us.deloitte.com>
2014-07-09 16:00:03 -0700

    added socket logic in server.js

--- server.js
+++ server.js
@@ -18,8 +18,54 @@ router.route('/cards')
   .get(function(req, res){
     res.json(allCard);
   });
+var rooms = {};
+io.on('connection', function(socket){
+  var addedUser = false,
+      roomOccupied;
 
+  socket.on('join room', function (roomNumber) {
+    if (roomNumber.hasOwnProperty('roomNumber')){
+      socket.join(roomNumber);
+      rooms[roomNumber][usernames][username] = socket.username;
+      ++rooms[roomNumber].numUsers;
+      roomOccupied = roomNumber;
+    }
+    else {
+      socket.join(roomNumber);
+      rooms[roomNumber] = {
+        usernames: {},
+        numUsers: 1
+      };
+      rooms[roomNumber].usernames[username] = socket.username;
+      roomOccupied = roomNumber;      
+    }
+    io.in(roomNumber).emit('join room', {
+        username: socket.username,
+        numUsers: rooms[roomNumber].numUsers
+    });
+  });
+
+  socket.on('disconnect', function(){
+    var room;
+    room = rooms[roomOccupied]
+    if (addedUser){
+      delete room.usernames[socket.username];
+      --room.numUsers;
+    }
+    io.in(roomOccupied).emit('user left',{
+      username: socket.username,
+      numUsers: numUsers
+    })
+    numUsers === 0 && (delete rooms[roomOccupied]);
+  });
+
+  socket.on('create user', function (username) {
+    socket.username = username;
+    addedUser = true;
+  });
+});
 app.use('/api', router);
+
 app.listen(process.env.PORT || 3000, function(){  //CONFIG.port
 	console.log("Server running on port " + 3000);
 });
